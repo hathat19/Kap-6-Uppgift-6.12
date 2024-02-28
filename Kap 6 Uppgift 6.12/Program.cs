bc845ae674784f30d123f465394036cfb3e94009
Hugo M <humy06001@utb.vaxjo.se>
2024-02-28 13:32:09 +0100

    Add project files.

---
+++ Kap 6 Uppgift 6.12/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Färger
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            //-------------------------------------------
+
+            Console.WriteLine("Hur hög ska triangeln vara?");
+            int height = ReadInt();
+            SkrivTriangel(height);
+
+        }
+
+        static int ReadInt()
+        {
+            int integer;
+            Console.WriteLine("Skriv in ett heltal:");
+
+            while (int.TryParse(Console.ReadLine(), out integer) == false)
+            {
+                Console.WriteLine("Du skrev inte in ett heltal. Försök igen.");
+            }
+            return integer;
+        }
+
+        static void SkrivTriangel(int height)
+        {
+            //i = radnumret
+            for (int i = 1; i <= height; i++)
+            {
+                //Mellanslagen
+                for(int j = 0; j < height - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                //Stjärnorna
+                //Stjärnorna följer ett mönster, där antal stjärnorna på varje rad = radens nummer*2 - 1
+                for (int j = 0; j < 2*i-1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+/*Skapa ett program som innehåller en metod som kan rita en triangel där användaren
+får bestämma triangelns höjd. Triangeln ska se ut så här om höjden är 3:
+  *
+ ***
+*****
+Om höjden är 4 ska den istället se ut så här:
+   *
+  ***
+ *****
+*******/
